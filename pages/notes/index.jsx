88c69169c3b10635ab7b6fb5c348ea78b03b4ba5
annamalaigokkul <annamalai.natarajan@askapplications.com>
2021-02-20 18:24:08 -0800

    Next JS course

---
+++ pages/notes/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Notes = () => (
+    <div>
+        <h1>Notes Index Path</h1>
+        <Link href="/notes/[id]" as={`/notes/1`}>
+            <a>
+                Note 1
+            </a>
+        </Link>
+    </div>
+);
+
+export default Notes;
